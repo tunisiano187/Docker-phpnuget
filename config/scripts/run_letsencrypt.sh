340a074a3fe6b69cf0b96e51f17dbe205cded512
Mark Gilbert <50398495+MarkProminic@users.noreply.github.com>
2021-05-08 12:36:14 -0500

    Update run_letsencrypt.sh

--- config/scripts/run_letsencrypt.sh
+++ config/scripts/run_letsencrypt.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-if (! [ -z "$STAGING" ]) then
-  echo "Using Let's Encrypt Staging environment..."
-  certbot -n --staging --expand --apache --agree-tos --email $WEBMASTER_MAIL "$@"
+
+## Use the User provided cert instead of running Let'sEncrypt
+if [test -f "/app/phpnuget/data/$SSLCRT"]  && [test -f "/app/phpnuget/data/$SSLKEY"] ; then
+    echo "$SSLCRT exists."
 else
-  echo "Using Let's Encrypt Production environment..."
-  certbot -n --expand --apache --agree-tos --email $WEBMASTER_MAIL "$@"
+    if (! [ -z "$STAGING" ]) then
+      echo "Using Let's Encrypt Staging environment..."
+      certbot -n --staging --expand --apache --agree-tos --email $WEBMASTER_MAIL "$@"
+    else
+      echo "Using Let's Encrypt Production environment..."
+      certbot -n --expand --apache --agree-tos --email $WEBMASTER_MAIL "$@"
+    fi
 fi
+
